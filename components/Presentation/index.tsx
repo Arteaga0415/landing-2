4da006c49b70c372f9196ee46480ed4f645cc2e1
Sebastian Arteaga Ronga <sarteaga0415@gmail.com>
2024-07-30 22:33:50 -0500

    feat: presentation added

---
+++ components/Presentation/index.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image"
+
+const Presentation = () => {
+  return (
+    <Image 
+      src={'/imagenes/Hoja Membretada Encabezado PNG.png'}
+      width={2484}
+      height={783}
+      alt="Hoja Membretada Encabezado"
+    />
+  )
+}
+
+export default Presentation;
