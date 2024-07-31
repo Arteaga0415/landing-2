53048b3ded2cce5853416910f390b2b8b1b0201a
Sebastian Arteaga Ronga <sarteaga0415@gmail.com>
2024-07-31 10:03:37 -0500

    feat: favicon created

--- app/(site)/head.tsx
+++ app/(site)/head.tsx
@@ -4,8 +4,7 @@ export default function Head() {
       <title> Ingeniería y Telecomunicaciones HL SAS</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Built with Next.js and TypeScript" />
-      <link rel="icon" href="/logos/logo PNG.png" />
-      {/* <link rel="icon" href="/images/favicon.ico" /> */}
+      <link rel="icon" href="/logos/favicon.ico" />
     </>
   );
 }
