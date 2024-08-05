6d7a20463ab4312570c428f5fd10e6ba2b45a85a
Sebastian Arteaga Ronga <sarteaga0415@gmail.com>
2024-08-05 13:10:51 -0500

    feat: contactenos title padding fixed

--- app/(site)/support/page.tsx
+++ app/(site)/support/page.tsx
@@ -11,8 +11,8 @@ export const metadata: Metadata = {
 
 const SupportPage = () => {
   return (
-    <section className="overflow-hidden pb-25 pt-45 lg:pb-32.5 lg:pt-50 xl:pb-37.5 xl:pt-55">
-      <h2 className="mx-auto mb-4 text-3xl font-bold text-black dark:text-white md:w-4/5 xl:text-sectiontitle3">
+    <section className="overflow-hidden py-25 lg:py-32.5 xl:py-37.5">
+      <h2 className="mx-auto flex justify-center mb-4 text-3xl font-bold text-black dark:text-white md:w-4/5 xl:text-sectiontitle3">
         Contactenos
       </h2>
       <Contact />
