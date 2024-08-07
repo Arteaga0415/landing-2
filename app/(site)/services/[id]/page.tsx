53dd11f84a2670c8af312cc3e5541d5a8f0ef233
Sebastian Arteaga Ronga <sarteaga0415@gmail.com>
2024-08-07 12:56:40 -0500

    feat: service page UI improved

--- app/(site)/services/[id]/page.tsx
+++ app/(site)/services/[id]/page.tsx
@@ -4,6 +4,7 @@ import { Metadata } from "next";
 import Image from "next/image";
 import BigService from "@/components/page-service";
 import Presentation from "@/components/Presentation";
+import SectionHeader from "@/components/Common/SectionHeader";
 
 export const metadata: Metadata = {
   title: "Blog Details Page - Solid SaaS Boilerplate",
@@ -13,43 +14,19 @@ export const metadata: Metadata = {
 const SingleBlogPage = async () => {
   return (
     <>
-      <section className="pb-20 pt-35 lg:pb-25 lg:pt-45 xl:pb-30 xl:pt-50">
+      <section className="pb-20 pt-30 lg:pb-25 lg:pt-35 xl:pb-30 xl:pt-40">
+        <div className="pb-10">
+          <SectionHeader
+            headerInfo={{
+              title: "Nuestros Servicios",
+              subtitle: "Nuestros Servicios",
+              description: `Telecomunicaciones y Energías Renovables, Solar y Eólica`,
+            }}
+          />
+        </div>
         <div className="mx-auto max-w-c-1390 px-4 pb-20 md:px-8 2xl:px-0">
           <div className="flex flex-col-reverse gap-7.5 lg:flex-row xl:gap-12.5">
-            <div className="md:w-1/2 lg:w-[32%]">
-              {/* <div className="animate_top mb-10 rounded-md border border-stroke bg-white p-9 shadow-solid-13 dark:border-strokedark dark:bg-blacksection">
-                <h4 className="mb-7.5 text-2xl font-semibold text-black dark:text-white">
-                  Servicios
-                </h4>
-
-                <ul>
-                  <li className="mb-3 transition-all duration-300 last:mb-0 hover:text-primary">
-                    <a href="#">Suministros De Equipos</a>
-                  </li>
-                  <li className="mb-3 transition-all duration-300 last:mb-0 hover:text-primary">
-                    <a href="#">Infraestructura</a>
-                  </li>
-                  <li className="mb-3 transition-all duration-300 last:mb-0 hover:text-primary">
-                    <a href="#">Conectividad Remota</a>
-                  </li>
-                  <li className="mb-3 transition-all duration-300 last:mb-0 hover:text-primary">
-                    <a href="#">Camaras de Vigilancia y Monitoreo</a>
-                  </li>
-                  <li className="mb-3 transition-all duration-300 last:mb-0 hover:text-primary">
-                    <a href="#">Enlaces punto a punto</a>
-                  </li>
-                  <li className="mb-3 transition-all duration-300 last:mb-0 hover:text-primary">
-                    <a href="#">Energía Solar</a>
-                  </li>
-                  <li className="mb-3 transition-all duration-300 last:mb-0 hover:text-primary">
-                    <a href="#">Energía Eólica</a>
-                  </li>
-                  <li className="mb-3 transition-all duration-300 last:mb-0 hover:text-primary">
-                    <a href="#">IPV 6</a>
-                  </li>
-                </ul>
-              </div> */}
-
+            <div className="md:w-1/2 lg:w-[32%] overflow-auto h-[800px] md:h-full">
               <RelatedService />
             </div>
             <BigService />
