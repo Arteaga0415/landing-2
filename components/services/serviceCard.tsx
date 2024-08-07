d65fe343292b3a4a043224ec79ba5977beabd67d
Sebastian Arteaga Ronga <sarteaga0415@gmail.com>
2024-08-07 12:21:39 -0500

    feat: service card data added and card styled

--- components/services/serviceCard.tsx
+++ components/services/serviceCard.tsx
@@ -8,29 +8,76 @@ import {
 import { 
   Card,
   CardContent,
-  
- } from "../card"
+
+} from "../card"
+import Image from "next/image"
+
+type ServiceData = {
+  title: string;
+  desc: string;
+  body: string;
+  img: string;
+}
+
+const serviceData: ServiceData[] = [
+  {
+    title: 'Telecomunicaciones',
+    desc: 'Servicios de Conectividad Remota Empresarial',
+    body: 'Ofrecemos servicios de conectividad remota para empresas, permitiendo la interconexión de múltiples sedes a través de soluciones de red avanzadas.',
+    img: '/imagenes/landing-4.jpg',
+  },
+  {
+    title: 'Energía Solar',
+    desc: 'Energía renovables',
+    body: 'Ofrecemos servicios de suministro, instalación y mantenimiento de equipos para granjas de energía solar.',
+    img: '/imagenes/landing-solar-2.jpg',
+  },
+  {
+    title: 'Energía Eólica',
+    desc: 'Energía renovables',
+    body: 'Prestamos servicios de suministro, instalación y mantenimiento de equipos para granjas de energía eólica.',
+    img: '/imagenes/landing-wind-2.jpeg',
+  },
+
+]
 
 export default function ServiceCard () {
   return (
-    <>
-      <Carousel className="w-full max-w-xs">
-        <CarouselContent>
-          {Array.from({ length: 5 }).map((_, index) => (
-            <CarouselItem key={index}>
+    <section className="overflow-hidden flex items-center justify-center pb-20 md:px-20 py-10 md:py-15 lg:py-20">
+      <Carousel opts={{align: "start"}} orientation="vertical" className="w-full">
+        <CarouselContent className="mt-1 h-[450px] md:h-[500px]">
+          {serviceData.map((el, index) => (
+            <CarouselItem className="pt-1" key={index}>
               <div className="p-1">
                 <Card>
-                  <CardContent className="flex aspect-square items-center justify-center p-6">
-                    <span className="text-4xl font-semibold">{index + 1}</span>
+                  <CardContent className="flex items-center justify-center p-6">
+                    <div className="flex flex-col">
+                      <div className="flex justify-between py-5">
+                        <h2 className="text-3xl font-semibold text-green1">{el.title}</h2>
+                        <h6 className="text-lg font-normal w-2/5">{el.desc}</h6>
+                      </div>
+                      <div className="relative">
+                        <Image
+                          src={el.img}
+                          width={700}  
+                          height={600}
+                          alt="imagen"  
+                          className="rounded-md"                 
+                        />
+                        <h6 className="text-justify text-lg font-normal w-2/5 absolute bottom-5 right-5">
+                          {el.body}
+                        </h6>
+                      </div>
+                    </div>
                   </CardContent>
                 </Card>
               </div>
             </CarouselItem>
-          ))}
+          )) }
         </CarouselContent>
         <CarouselPrevious />
         <CarouselNext />
       </Carousel>
-    </>
+    </section>
   )
 }
--- components/video/index.tsx
+++ components/video/index.tsx
@@ -12,11 +12,11 @@ export function Video() {
           />
           Tu navegador no tiene soporte para el Video.
         </video>
-        <div className="absolute top-0 left-0 m-4 text-white w-2/5 bg-opacity-50 px-2 py-8 rounded">
-          <h1 className="md:text-2xl font-bold text-white">Con más de 20 años de experiencia en telecomunicaciones, tecnología educativa y energías renovables.</h1>
+        <div className="absolute top-0 left-0 m-4 text-white w-2/5 bg-opacity-50 md:px-2 md:py-8 rounded">
+          <h1 className="text-sm md:text-2xl font-semibold md:font-bold text-white">Con más de 20 años de experiencia en telecomunicaciones, tecnología educativa y energías renovables.</h1>
         </div>
-        <div className="flex text-end absolute top-0 right-0 m-4 text-white w-1/2 bg-opacity-50 py-4 rounded">
-          <h1 className="md:text-6xl font-bold text-white">Ingeniería y Telecomunicaciones</h1>
+        <div className="flex text-end absolute top-0 right-0 m-4 text-white w-1/2 bg-opacity-50 md:py-4 rounded">
+          <h1 className="text-lg md:text-6xl font-bold text-white">Ingeniería y Telecomunicaciones</h1>
         </div>
       </div>
     </section>
