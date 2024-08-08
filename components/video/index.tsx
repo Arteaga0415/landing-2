0da4ea805d0cbe53c743c1ceded9f7e22924465a
Sebastian Arteaga Ronga <sarteaga0415@gmail.com>
2024-08-08 10:31:50 -0500

    feat: testing andriod 2

--- components/video/index.tsx
+++ components/video/index.tsx
@@ -34,8 +34,8 @@ export function Video() {
         //   height={932}
         //   alt="Imagen"
         // /> 
-        <video className="w-full object-cover" loop autoPlay playsInline muted>
-          <source src="/videos/landing-1.mp4" />
+        <video className="w-full object-cover" loop autoPlay webkit-playsinline playsInline muted>
+          <source src="/videos/landing-1.mp4" type="video/mp4" />
           <track
             src="/path/to/captions.vtt"
             kind="subtitles"
