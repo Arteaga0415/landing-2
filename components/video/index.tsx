34960259fc68a9ef4c09cbb5a9d06c88895f5396
Sebastian Arteaga Ronga <sarteaga0415@gmail.com>
2024-08-08 10:50:15 -0500

    feat: finished MVP

--- components/video/index.tsx
+++ components/video/index.tsx
@@ -2,7 +2,6 @@
 
 import { useState, useEffect } from "react";
 import Image from "next/image";
-import VideoComponent from "./blob-video";
 
 export function Video() {
   const [isAndroid, setIsAndroid] = useState(false);
@@ -18,9 +17,23 @@ export function Video() {
     <section className="pt-30">
       <div className="relative overflow-hidden">
         {!isAndroid ? 
-          <VideoComponent fileName={'landing-1.mp4'} />
+          <video className="w-full object-cover" loop autoPlay playsInline muted>
+            <source src="/videos/landing-1.mp4" type="video/mp4" />
+            <track
+              src="/path/to/captions.vtt"
+              kind="subtitles"
+              srcLang="en"
+              label="English"
+            />
+            Tu navegador no tiene soporte para el Video.
+          </video>
         : 
-        <VideoComponent fileName={'landing-1.mp4'} />
+        <Image
+          src={'/imagenes/video-fallback-1.jpg'}
+          width={3175}
+          height={932}
+          alt="Imagen"
+        /> 
         }
         <div className="absolute top-0 left-0 m-4 text-white w-2/5 bg-opacity-50 md:px-2 md:py-8 rounded">
           <h1 className="text-sm md:text-2xl font-semibold md:font-bold text-white">Con más de 20 años de experiencia en telecomunicaciones, tecnología educativa y energías renovables.</h1>
